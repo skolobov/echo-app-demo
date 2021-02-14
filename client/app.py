f31d40606505e5c385c5387aca2c84979d28ba94
Sergei Kolobov <sergei@kolobov.com>
2021-02-14 12:44:35 +0300

    feat: initial implementation of echo client

---
+++ client/app.py
@@ -0,0 +1,30 @@
+import os, requests
+
+from flask import Flask, request, render_template, redirect
+from wtforms import SelectField, StringField, TextAreaField, SubmitField
+from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(12).hex()
+Bootstrap(app)
+
+class RequestForm(FlaskForm):
+    method = SelectField("Method", choices=["GET", "OPTIONS", "HEAD", "POST", "PUT", "PATCH", "DELETE"])
+    body = StringField("Body")
+    submit = SubmitField('Send')
+    response = TextAreaField("Response from Backend")
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = RequestForm(request.form)
+
+    if request.method == 'POST' and form.validate():
+        response = requests.request(form.method.data,"http://localhost:1323",data=form.body.data)
+        form.response.data = response.content.decode('UTF-8')
+        redirect('index')
+
+    return render_template('index.html', form=form)
+
+if __name__ == "__main__":
+    app.run()
