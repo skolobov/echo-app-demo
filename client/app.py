0d7fba3e36c81a680c9f948bc0f3d3a71e63765f
Sergei Kolobov <sergei@kolobov.com>
2021-02-15 05:18:05 +0300

    style: fix Flake8 linter errors

--- client/app.py
+++ client/app.py
@@ -1,4 +1,5 @@
-import os, requests
+import os
+import requests
 
 from flask import Flask, request, render_template, redirect
 from wtforms import SelectField, StringField, TextAreaField, SubmitField
@@ -8,24 +9,29 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(12).hex()
 Bootstrap(app)
-backend_url = os.getenv('BACKEND_URL','http://localhost:1323')
+backend_url = os.getenv('BACKEND_URL', 'http://localhost:1323')
+HTTP_METHODS = ["GET", "OPTIONS", "HEAD", "POST", "PUT", "PATCH", "DELETE"]
+
 
 class RequestForm(FlaskForm):
-    method = SelectField("Method", choices=["GET", "OPTIONS", "HEAD", "POST", "PUT", "PATCH", "DELETE"])
+    method = SelectField("Method", choices=HTTP_METHODS)
     body = StringField("Body")
     submit = SubmitField('Send')
     response = TextAreaField("Response from Backend")
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = RequestForm(request.form)
 
     if request.method == 'POST' and form.validate():
-        response = requests.request(form.method.data, backend_url, data=form.body.data)
+        response = requests.request(form.method.data,
+                                    backend_url, data=form.body.data)
         form.response.data = response.content.decode('UTF-8')
         redirect('index')
 
     return render_template('index.html', form=form)
+
 
 if __name__ == "__main__":
     app.run()
